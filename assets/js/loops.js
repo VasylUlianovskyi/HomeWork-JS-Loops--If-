d1293f4470db3b7c2e91ebb60982b7b58559a228
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-05-13 19:11:11 +0300

    Finish loops tasks

--- assets/js/loops.js
+++ assets/js/loops.js
@@ -8,35 +8,59 @@ let currentNumber = 25;
 const TOTAL_NUMBER = 0;
 
 while (currentNumber >= TOTAL_NUMBER) {
-  document.write(`<p>${currentNumber}; </p>`);
+  document.write(`<span>${currentNumber}; </span>`);
   console.log(currentNumber);
   currentNumber--;
 }
 
 // do... while;
 
+let count = 25;
+const FINAL_COUNT = 0;
+
+do {
+  document.write(`<p><span>${count--}</span></p>`);
+  console.log(count);
+} while (count >= FINAL_COUNT);
+
 // for
 
-// 2. Вивести числа від 10 до 50, які кратні 5.
+let stopCount = 0;
+for (let startCount = 25; startCount >= stopCount; startCount--) {
+  document.write(`<p>#${startCount}</p>`);
+}
+
+// 2. ВИВЕСТИ ЧИСЛА ВІД 10 ДО 50, ЯКІ КРАТНІ 5.
 
 document.write(`<h3>Вивести числа від 10 до 50, які кратні 5
 </h3>`);
 
+// while
 let number = 10;
 const TOTAL_COUNT = 50;
 
 while (number <= TOTAL_COUNT) {
-  document.write(`<p>${number}; </p>`);
-  console.log(number);
+  document.write(`<span>${number}, </span>`);
   number += 5;
 }
-console.log("Success");
 
 // do... while;
 
+let value = 10;
+
+do {
+  document.write(`<p>${value}, </p>`);
+  value += 5;
+} while (value <= 50);
+
 // for
 
+for (let i = 10; i <= 50; i += 5) {
+  document.write(`<p>#${i}</p>`);
+}
+
 // 3. *Знайти суму чисел в межах від 1 до 100.
+// while
 
 document.write(`<h3>Знайти суму чисел в межах від 1 до 100.
 </h3>`);
@@ -49,9 +73,25 @@ while (firstNumber <= LAST_NUMBER) {
   sum += firstNumber;
   firstNumber++;
 }
-console.log("Сума чисел в межах від 1 до 100 = ", sum);
 document.write("Сума чисел в межах від 1 до 100 = ", sum);
 
 // do... while;
 
+let a = 1;
+let mult = 0;
+
+do {
+  mult += a;
+  a++;
+} while (a <= 100);
+document.write(" Сума чисел в межах від 1 до 100 = ", mult);
+
 // for
+
+let finalNumber = 100;
+let sumValue = 0;
+
+for (let b = 1; b <= finalNumber; b++) {
+  sumValue += b;
+}
+document.write(" Сума чисел в межах від 1 до 100 = ", sumValue);
---
+++ assets/js/task4.js
@@ -0,0 +1,30 @@
+// Задане значення змінної isLogin (залогінився користувач чи ні).
+// Сформувати header наступним чином:
+// зліва - логотип (зображення на ваш смак). Класично лого є посиланням на головну сторінку.
+// справа - або кнопки Login та Register (якщо користувач не залогінився)
+//                 або фото і ім'я користувача (якщо користувач залогінився)
+// *додатково застилізувати фото користувача: за значанням isMale для чоловіка зробити синю рамку, для жінки червону, при незаданому значенні isMale - сіру.
+
+const isLogin = true;
+
+if (isLogin) {
+  document.write(`
+  <header>
+  <div class="logo">LOGO</div>
+  <div class="user-info">
+    <p>Joe Dou</p>
+    <img src="" alt="">
+  </div>
+</header>
+  `);
+} else {
+  document.write(`
+  <header>
+  <div class="logo">LOGO</div>
+  <div class="btn-wrap">
+    <button>Login</button>
+    <button>Register</button>
+  </div>
+</header>
+`);
+}
